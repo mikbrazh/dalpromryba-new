69a27e70c6f41919c51211c96862cb3cccd9f6b6
Mikhail Brazhnik <mikbrazh@gmail.com>
2024-01-24 17:20:49 +0300

    production

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,126 @@
-function scrollProduct()
-{
+function scrollProduct() {
     let offsetTop = document.getElementById('product').offsetTop;
-    $('body,html').animate({scrollTop: offsetTop}, 1000);
+    $('body,html').animate({ scrollTop: offsetTop }, 1000);
 }
 
-// $(document).ready(() =>
-// {
+$(document).ready(() => {
+    /* ======= stages-slider START ======= */
+    if (document.querySelector('.stages-slider')) {
 
-// })
+        /* ======= swiper START ======= */
+        const stagesSpratsSwiper = new Swiper('.stages-sprats-swiper', {
+            // Default parameters
+            direction: 'horizontal',
+            loop: true,
+            speed: 500,
+            slidesPerView: 1,
+            navigation: {
+                nextEl: '.stages-sprats-swiper-button-prev',
+                prevEl: '.stages-sprats-swiper-button-next',
+            },
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+        });
 
-/* ======= stages-slider START ======= */
-if (document.querySelector('.stages-slider')) {
-/* ======= swiper START ======= */
-const stagesSpratsSwiper = new Swiper('.stages-sprats-swiper', {
-    // Default parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 500,
-    slidesPerView: 1,
-    navigation: {
-    nextEl: '.stages-sprats-swiper-button-prev',
-    prevEl: '.stages-sprats-swiper-button-next',
-    },
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+        const stagesTunaSwiper = new Swiper('.stages-tuna-swiper', {
+            // Default parameters
+            direction: 'horizontal',
+            loop: true,
+            speed: 500,
+            slidesPerView: 1,
+            navigation: {
+                nextEl: '.stages-tuna-swiper-button-prev',
+                prevEl: '.stages-tuna-swiper-button-next',
+            },
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+        });
+        /* ======= swiper END ======= */
+
+        // Написать для всех видео
+        // Убираем полоску при проигрывании, при паузе возращаем обратно
+        // Убираем кнопку плей при проигрывании, при паузе возращаем обратно 
+        // Убираем постер при проигрывании, при паузе возращаем обратно
+        // Делаем пагинацию
+        // ПОстер на все ширину свайпера
+
+        const $__stagesSlides = document.querySelectorAll('.stages-slider__slide');
+
+        $__stagesSlides.forEach((slide) => {
+
+            const $__stagesSwiper = slide.parentElement.parentElement;
+            const $__sliderVideo = slide.querySelector('.stages-slider__video');
+            const $__sliderPoster = slide.querySelector('.stages-slider__video-poster');
+            const $__sliderTitleBlock = slide.querySelector('.stages-slider__title-block');
+            const $__sliderButtonPrev = $__stagesSwiper.querySelector('.stages-slider__button-prev');
+            const $__sliderButtonNext = $__stagesSwiper.querySelector('.stages-slider__button-next');
+            const $__sliderPlayButton = slide.querySelector('.stages-slider__play-button');
+            const $__pagination = $__stagesSwiper.querySelector('.swiper-pagination');
+            // const $__paginationBullets = $__stagesSwiper.querySelectorAll('.swiper-pagination-bullet');
+
+            slide.addEventListener('click', (event) => {
+                if (event.target == $__sliderPlayButton) {
+                    $__sliderVideo.play();
+
+                    $__sliderVideo.controls = true;
+
+                    $__sliderPoster.style.display = 'none';
+                    $__sliderTitleBlock.style.display = 'none';
+                    $__sliderButtonPrev.style.display = 'none';
+                    $__sliderButtonNext.style.display = 'none';
+                    $__sliderPlayButton.style.display = 'none';
+                    $__pagination.style.display = 'none';
+
+
+                    $__sliderVideo.addEventListener('ended', (event) => {
+                        $__sliderPoster.style.display = 'block';
+                        $__sliderTitleBlock.style.display = 'block';
+                        $__sliderButtonPrev.style.display = 'block';
+                        $__sliderButtonNext.style.display = 'block';
+                        $__sliderPlayButton.style.display = 'block';
+                        $__pagination.style.display = 'block';
+                    }, false);
+                }
+            }, false);
+
+
+        });
+
+        document.addEventListener('click', (event) => {
+            // Проверка на проигрывание видео
+            // console.log(event.target);
+            // console.log(event.target.classList.contains('stages-slider__play-button'));
+
+            if (!event.target.classList.contains('stages-slider__play-button')) {
+                const $__sliderVideos = document.querySelectorAll('.stages-slider__video');
+
+                const $__sliderButtonPrev = document.querySelector('.stages-slider__button-prev');
+            const $__sliderButtonNext = document.querySelector('.stages-slider__button-next');
+            const $__sliderPlayButton = document.querySelector('.stages-slider__play-button');
+            const $__pagination = document.querySelector('.swiper-pagination');
+            const $__sliderTitleBlock = document.querySelector('.stages-slider__title-block');
+
+                $__sliderVideos.forEach((video) => {
+
+                    video.pause();
+
+                    // Убрать controls
+                    $__sliderButtonPrev.style.display = 'block';
+                    $__sliderButtonNext.style.display = 'block';
+                    $__pagination.style.display = 'block';
+                    $__sliderPlayButton.style.display = 'block';
+                    $__sliderTitleBlock.style.display = 'block';
+                    
+                });
+
+            }
+
+
+        }, false);
+    }
 });
-
-const stagesTunaSwiper = new Swiper('.stages-tuna-swiper', {
-    // Default parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 500,
-    slidesPerView: 1,
-    navigation: {
-    nextEl: '.stages-tuna-swiper-button-prev',
-    prevEl: '.stages-tuna-swiper-button-next',
-    },
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-});
-/* ======= swiper END ======= */
-}
 /* ======= stages-slider END ======= */
